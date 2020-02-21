e0d1c4bcf90d7ee9f2ed17307da0d431ba9b0c90
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-02-21 14:25:21 +0530

    Set up packaging

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='glitch_this',
+    version='0.0.1',
+    author='TotallyNotChase',
+    author_email='44284917+TotallyNotChase@users.noreply.github.com',
+    description='A package to glitch images',
+    url='https://github.com/TotallyNotChase/Glitch-and-Gif',
+    packages=find_packages(),
+    install_requires=[
+        'Pillow>=6.2.1',
+        'numpy>=1.18.1',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: MIT Licence",
+    ],
+)
