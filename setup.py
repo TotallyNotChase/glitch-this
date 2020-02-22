8e187c45677f7b59f8c8a8128e25f6bef5d45b5f
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-02-22 18:53:26 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
     author_email='44284917+TotallyNotChase@users.noreply.github.com',
     description='A package to glitch images',
     long_description=readme,
+    long_description_content_type='text/markdown',
     url='https://github.com/TotallyNotChase/Glitch-and-Gif',
     packages=find_packages(),
     install_requires=[
