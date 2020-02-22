751d6bb457856227c25103a746b27dee558f2794
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-02-22 18:19:20 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'R') as file:
+    readme = file.read()
+
 setup(
     name='glitch_this',
     version='0.0.1',
     author='TotallyNotChase',
     author_email='44284917+TotallyNotChase@users.noreply.github.com',
     description='A package to glitch images',
+    long_description=readme,
     url='https://github.com/TotallyNotChase/Glitch-and-Gif',
     packages=find_packages(),
     install_requires=[
