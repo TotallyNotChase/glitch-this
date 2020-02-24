b91128b75b8c2ab44f7dcf49c511725a9e622320
Chase <44284917+TotallyNotChase@users.noreply.github.com>
2020-02-24 13:04:48 +0530

    Add more detailed test_script

--- test_script.py
+++ test_script.py
@@ -3,29 +3,25 @@ from random import randint
 from time import time
 from glitch_this import ImageGlitcher
 
-def main():
+def test_loop():
+    # A method to stress test
     count = 0
     sum = 0
     try:
-        if os.path.isdir('Collections'):
-            shutil.rmtree('Collections')
-        os.mkdir('Collections')
         with open('Collections/imglog.txt', 'w') as logtxt:
             while(1):
                 t0 = time()
                 level = randint(1, 10)
                 """
-                 glitch_image() will return a PIL.Image.Image object if gif is set to False
-                 Otherwise it returns a list of Image objects
-                 Make sure to supply the full/relative path to the image as well as glitch level
-                 Set `color_offset` to True for color channel offset, otherwise leave it
-                 Set `scan_lines` to True for scan lines effect, otherwise leave it empty
-                 Set `gif` to True if you want a list of frames to return instead
-                 Set `frames` to an int value, denoting number of frames
+                 Example of getting a glitched image and saving it,
+                 with all default params
+
+                 glitch_image() will return a PIL.Image.Image object
+                 since gif is set to False
 
                  Check DOCS for more info!
                 """
-                glitch_img = glitcher.glitch_image('test.png', level, color_offset=True)
+                glitch_img = glitcher.glitch_image('test.png', level)
                 # You can then save it or do anything else you want with it
                 glitch_img.save('Collections/glitched_test_{}.png'.format(str(count)))
                 t1 = time()
@@ -36,20 +32,99 @@ def main():
     except KeyboardInterrupt:
         print('Average time: ' + str(sum / count))
 
-def main_alt():
-    glitch_img = glitcher.glitch_image('test.png', 2, color_offset=True, gif=True, frames=2)
-    if os.path.isdir('Collections'):
-        shutil.rmtree('Collections')
-    os.mkdir('Collections')
-    glitch_img[0].save('Collections/glitched_test.gif',
+def test_image_to_image():
+    """
+     Example of getting a glitched Image and saving it
+
+     We use glitch_level = 2 in this example
+     You may change this to whatever you'd like
+    """
+
+    # All default params(i.e color_offset = False, scan_lines = False)
+    glitch_img = glitcher.glitch_image('test.png', 2)
+    glitch_img.save('Collections/glitched_test_default.png')
+
+    # Now try with scan_lines set to true
+    glitch_img = glitcher.glitch_image('test.png', 2, scan_lines=True)
+    glitch_img.save('Collections/glitched_test_scan.png')
+
+    # Now try with color_offset set to true
+    glitch_img = glitcher.glitch_image('test.png', 2, color_offset=True)
+    glitch_img.save('Collections/glitched_test_color.png')
+
+    # How about all of them?
+    glitch_img = glitcher.glitch_image('test.png', 2, color_offset=True, scan_lines=True)
+    glitch_img.save('Collections/glitched_test_all.png')
+
+
+def test_image_to_gif():
+    """
+     Example of getting a glitched GIF and saving it
+
+     We use glitch_level = 2 in this example
+     We also are making infinitely looping GIFs
+     i.e loop = 0
+     You may change these to whatever you'd like
+    """
+
+    DURATION = 200      # Set this to however many centiseconds each frame should be visible for
+
+    # All default params (i.e FRAMES = 23, color_offset = False, scan_lines = False)
+    glitch_imgs = glitcher.glitch_image('test.png', 2, gif=True)
+    glitch_imgs[0].save('Collections/glitched_test_default.gif',
                         format='GIF',
-                        append_images=glitch_img[1:],
+                        append_images=glitch_imgs[1:],
                         save_all=True,
-                        duration=200,
+                        duration=DURATION,
+                        loop=0)
+    # Now try with scan_lines set to true
+    glitch_imgs = glitcher.glitch_image('test.png', 2, gif=True, scan_lines=True)
+    glitch_imgs[0].save('Collections/glitched_test_scan.gif',
+                        format='GIF',
+                        append_images=glitch_imgs[1:],
+                        save_all=True,
+                        duration=DURATION,
+                        loop=0)
+
+    # Now try with color_offset set to true
+    glitch_imgs = glitcher.glitch_image('test.png', 2, gif=True, color_offset=True)
+    glitch_imgs[0].save('Collections/glitched_test_color.gif',
+                        format='GIF',
+                        append_images=glitch_imgs[1:],
+                        save_all=True,
+                        duration=DURATION,
+                        loop=0)
+
+    # Now try with 10 frames
+    glitch_imgs = glitcher.glitch_image('test.png', 2, gif=True, frames=10)
+    glitch_imgs[0].save('Collections/glitched_test_frames.gif',
+                        format='GIF',
+                        append_images=glitch_imgs[1:],
+                        save_all=True,
+                        duration=DURATION,
+                        loop=0)
+
+    # How about all of the above?
+    glitch_imgs = glitcher.glitch_image('test.png', 2, gif=True, scan_lines=True, color_offset=True, frames=10)
+    glitch_imgs[0].save('Collections/glitched_test_all.gif',
+                        format='GIF',
+                        append_images=glitch_imgs[1:],
+                        save_all=True,
+                        duration=DURATION,
                         loop=0)
 
 if __name__=='__main__':
     # Create the ImageGlitcher object
     glitcher = ImageGlitcher()
-    main_alt()
-    
+    if os.path.isdir('Collections'):
+        shutil.rmtree('Collections')
+    os.mkdir('Collections')
+    print('Testing image to image glitching....')
+    test_image_to_image()
+    print('Done!')
+    print('Testing image to GIF glitching....')
+    test_image_to_gif()
+    print('Done!')
+    print('Testing infinite stress test.....\nNOTE: Use ctrl+c to stop the test')
+    test_loop()
+    print('Done!')
